a7ba22a6527689abc61bd8b59f76f281a4a4a99e
kimjimin1101 <abcpanda@likelion.org>
2021-06-04 00:41:29 +0900

    first commit

---
+++ signup/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.h
+class Signup(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    pub_date = models.DateTimeField()
+    email = models.EmailField(max_length=100)
+    introduce = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+   
